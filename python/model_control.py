e9db192c7aa871013fb327f79e403217138da738
1alex2lee <lal19@ic.ac.uk>
2023-03-12 22:34:43 +0000

    Revert "configurable models and funcs for new main ui"
    This reverts commit 20f3e0e7a37f6e8d42d6148c435b2ea450ec9ae2.

--- python/model_control.py
+++ python/model_control.py
@@ -10,28 +10,28 @@ batch_size = 4
 
 model_dir = ""
 
-model = ResUNet(num_channel,batch_size)
-model = model.to(device)
-
 # print("thinning model loaded")
 # model.load_state_dict(torch.load(os.path.join('CarDoorModel','Door_ResSEUNet_1408_B4_2000_LRFix0.0002_E6B6D6_Th.pkl'),map_location=device))
 # model.eval()
 
 
-def select_process_material (process, material):
-    global model_dir
-
-    model_dir = os.path.join(os.getcwd(), "python", process, material)
-    print("model directory is ", model_dir)
-
-
-def select_model_type (model_type):
+def load_model (process, material, model_type):
     global model, model_dir
 
-    model.load_state_dict(torch.load(os.path.join(model_dir, model_type), map_location=device))
+    model_dir = os.path.join(os.getcwd(), "models", process, material)
+
+    if model_type == "Thinning":
+        model = ResUNet(num_channel,batch_size, 5, 4, 2)
+        model = model.to(device)
+    else:
+        model = ResUNet(num_channel,batch_size, 3, 6, 3)
+        model = model.to(device)
+
+    model_dir = os.path.join(model_dir, model_type)
+    model.load_state_dict(torch.load(model_dir, map_location=device))
     model.eval()
 
-    print(model_type + " model loaded")
+    print(model_dir + " loaded")
 
 
 def predict(input):
@@ -40,7 +40,7 @@ def predict(input):
 
     decoded = model(input)
 
-    return decoded.cpu().detach().numpy().reshape(2,256,384)
+    return decoded.cpu().detach().numpy().reshape(-1,256,384)
 
 
 def train (epochs, window):
@@ -57,3 +57,8 @@ def train (epochs, window):
 def begin_training (window, name, material, target, epochs, batch_size, input_dir, output_dir):
     threading.Thread(target=train, args=(20, window)).start()
 
+
+def get_model_info ():
+    global model_dir
+    # print(model_dir.split("/")[-3])
+    return model_dir.split("/")[-1], model_dir.split("/")[-2], model_dir.split("/")[-3]
